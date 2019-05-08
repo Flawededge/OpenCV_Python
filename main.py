03de9b6ae2e7718a22930340d96d60cf0eda4ede
Flawededge <crdiggle@gmail.com>
2019-05-08 18:02:10 +1200

    GUI and detection is fully working with a couple tweaks to go

--- main.py
+++ main.py
@@ -1,51 +1,64 @@
 # # Compile code
 # pyinstaller -y -w -i "C:/Users/Ben/PycharmProjects/OpenCV_Python/example.ico" --add-data "C:/Users/Ben/PycharmProjects/OpenCV_Python/example.png";"." --add-data "C:/Users/Ben/PycharmProjects/OpenCV_Python/HSV.png";"." "C:/Users/Ben/PycharmProjects/OpenCV_Python/main.py"
 
+import os
+os.system("pyuic5 mainwindow.ui > mainwindow.py")
+
+import importlib
 import sys  # Forgot where I actually used this, but it's here
-from PyQt5 import QtWidgets, QtGui  # QT stuff
+from PyQt5 import QtWidgets, QtGui, QtCore  # QT stuff
 from mainwindow import Ui_OpenCVThresholder  # Import UI
-import cv2  # OpenCV
+from cv2 import *  # OpenCV
 import tkinter as tk            # For file dialog
 from tkinter import filedialog  # For file dialog
+import processing
+import numpy as np
 
 
 class MainPlotGui(Ui_OpenCVThresholder):
 
     # The class variables which are used to pass things around
-    currentImage = "example.png"
-    curFileImage = None
-    mask = None
+    currentFilename = "referenceImage.png"
+    inputImage = None
+    processedImage = None
     loading = 0
+    path = None
 
-    def __init__(self, dialog):
-        Ui_OpenCVThresholder.__init__(self)  # Set up the GUI
+    def __init__(self, dialog):  # Initialization function
+        # Set up the GUI
+        Ui_OpenCVThresholder.__init__(self)
         self.setupUi(dialog)
+        # Load the image and put onto the GUI
+        self.load_image()
+        self.process_image()
 
-        self.lColorBar.setPixmap(QtGui.QPixmap("HSV.png"))  # For the pretty bar in the middle of the interface
+        # Update timer to occasionally process the image
+        self.checkThreadTimer = QtCore.QTimer()
+        self.checkThreadTimer.setInterval(1000)  # .5 seconds
 
-        self.load_image()  # Load the image and put onto the GUI
-        self.update_outline()
+        self.checkThreadTimer.timeout.connect(self.process_image)
+        self.checkThreadTimer.start(1000)
 
         # Connecting GUI elements to functions
-        self.sLower.sliderMoved.connect(self.upper_slider)  # Attach sliders to update the input image
-        self.sUpper.sliderMoved.connect(self.lower_slider)
-        self.bClear.pressed.connect(self.clear_mask)  # Attach clearing
-        self.bAdd.pressed.connect(self.add_mask)
         self.bLoad.pressed.connect(self.load_file)
         self.bSave.pressed.connect(self.save_file)
 
+    def process_image(self):
+        importlib.reload(processing)  # Reload the processing file to make testing easier
+        self.processedImage, sausages = processing.process(self.inputImage)  # Run the function
+        self.processedImage, self.path = processing.path(self.processedImage, sausages)
+        self.disp_image(self.processedImage, 1)  # Show final output
+
     def load_image(self):  # Loads image from a filename
-        if not (self.currentImage.endswith('.png') or self.currentImage.endswith('.jpg')):
+        if not (self.currentFilename.endswith('.png') or self.currentFilename.endswith('.jpg')):
             print("Incorrect file type!")
             return
-        print(f"Loading '{self.currentImage}'")
-        self.curFileImage = cv2.imread(self.currentImage)  # Load the image
-        self.mask = None
-        self.curFileImage = cv2.cvtColor(self.curFileImage, cv2.COLOR_BGR2HSV)  # Convert to HSV
-        self.disp_image(self.curFileImage, 0)  # Display the image on the left side
+        print(f"Loading '{self.currentFilename}'")
+        self.inputImage = imread(self.currentFilename, IMREAD_COLOR)  # Load the image
+        self.disp_image(cvtColor(self.inputImage, COLOR_BGR2RGB), 0)  # Display the image on the left side
+        self.process_image()  # Process the image, which displays it on the right
 
     def disp_image(self, image, window):  # Shows the given image on 0 (input) or 1 (output)
-        image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)  # Convert to RGB to display
         pix = QtGui.QPixmap(QtGui.QImage(image, image.shape[1], image.shape[0],
                                          image.shape[1] * 3, QtGui.QImage.Format_RGB888))
         if window:
@@ -53,58 +66,25 @@ class MainPlotGui(Ui_OpenCVThresholder):
         else:
             self.inImage.setPixmap(pix)
 
-    def upper_slider(self):  # Function to keep the slider values consistent
-        if self.sLower.value() > self.sUpper.value():
-            self.sUpper.setValue(self.sLower.value())
-        self.update_outline()
-
-    def lower_slider(self):  # Function to keep the slider values consistent
-        if self.sLower.value() > self.sUpper.value():
-            self.sLower.setValue(self.sUpper.value())
-        self.update_outline()
-
-    def update_outline(self, update_global=0):
-        thresh = cv2.inRange(self.curFileImage, (self.sLower.value(), 0, 0), (self.sUpper.value(), 255, 255))  # Threshold image
-
-        if update_global:  # If add button was pressed
-            if update_global == 1:
-                if self.mask is None:  # If no global mask currently exists
-                    self.mask = thresh  # Set global mask
-                else:  # If there is a global mask
-                    self.mask = cv2.bitwise_or(thresh, self.mask)  # Do a bitwise or to add the current mask to the global one
-
-            tmp = cv2.bitwise_and(self.curFileImage, self.curFileImage, mask=self.mask)  # Use the global mask to generate image
-            self.disp_image(tmp, 1)  # Display the image on the output panel
-
-        tmp = cv2.bitwise_and(self.curFileImage, self.curFileImage, mask=thresh)  # Use the current mask to generate image
-        self.disp_image(tmp, 0)  # Display the image on the input panel
-
-    def clear_mask(self):
-        self.mask.fill(0)
-        self.update_outline(2)
-
-    def add_mask(self):
-        self.update_outline(1)
-
-    def load_file(self):
+    def load_file(self):  # Opens a file dialog to get a filename
         if self.loading:
             return
         self.loading = 1
 
         root = tk.Tk()
         root.withdraw()
-        self.currentImage = filedialog.askopenfilename()
+        self.currentFilename = filedialog.askopenfilename()
         self.load_image()
         self.loading = 0
 
-    def save_file(self):
-        tmp = cv2.bitwise_and(self.curFileImage, self.curFileImage, mask=self.mask)
-        tmp = cv2.cvtColor(tmp, cv2.COLOR_HSV2BGR)  # Convert to RGB to save
-        cv2.imwrite("output.png", tmp)
+    def save_file(self):  # Saves the processedImage to output.png
+        imwrite("output.png", self.processedImage)
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    dialog = QtWidgets.QDialog()
+    dialog = QtWidgets.QDialog(flags=(QtCore.Qt.WindowMaximizeButtonHint | QtCore.Qt.WindowMinimizeButtonHint |
+                                      QtCore.Qt.WindowCloseButtonHint))
 
     prog = MainPlotGui(dialog)
 
---
+++ processing.py
@@ -0,0 +1,68 @@
+# This is here so it can be retroactively loaded
+from cv2 import *
+import numpy as np
+import timeit
+import tsp
+
+
+def process(image, logging=0):
+    start = timeit.default_timer()
+    new_image = inRange(image, (0, 0, 150), (255, 255, 245))  # Value thresholding is the easiest first step
+    new_image = medianBlur(new_image, 13)
+    new_image = morphologyEx(new_image, MORPH_OPEN, np.ones((13, 10)), iterations=2)
+    new_image = morphologyEx(new_image, MORPH_CLOSE, np.ones((13, 10)), iterations=5)
+
+    contours, hierarchy = findContours(new_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    new_image = cvtColor(image, COLOR_BGR2RGB)
+    # new_image = drawContours(new_image, contours, -1, (0, 255, 0), 3)
+    sausages = []
+    for i in contours:  # Draw rectangles
+        # Check if box is sausage shape/size
+        x, y, width, height = boundingRect(i)
+
+        if height > 400 and width < 200:
+            rectangle(new_image, (x, y), (x + width, y + height), (0, 255, 0), 5)
+            print(f"Sausage: Wid:{height}  \tHei:{width}")
+            sausages.append([int(x + (width / 2)), int(y + (height / 2))])
+        else:
+            print("")
+            rectangle(new_image, (x, y), (x + width, y + height), (255, 0, 0), 5)
+            print(f"Not sausage: Wid:{height}  \tHei:{width}")
+
+    crosshair_size = 30
+    for i in sausages:
+        # Draw crosshairs in the centers of the sausages
+        line(new_image, (i[0] + crosshair_size, i[1]), (i[0] - crosshair_size, i[1]),
+             (0, 0, 255), thickness=3, lineType=8, shift=0)
+        line(new_image, (i[0], i[1] + crosshair_size), (i[0], i[1] - crosshair_size),
+             (0, 0, 255), thickness=3, lineType=8, shift=0)
+
+    end = timeit.default_timer()
+    print(f"Processing done in: {end - start}")
+    return new_image, sausages
+
+
+def path(image, sausages):
+    try:
+        sausages.insert(0, [0, 0])
+        start = timeit.default_timer()
+
+        shortest, order = tsp.tsp(sausages)
+        prev = None
+        for cur in order:
+            if prev is None:
+                prev = cur
+            else:
+                line(image, (sausages[prev][0], sausages[prev][1]), (sausages[cur][0], sausages[cur][1]),
+                     (255, 0, 255), thickness=3, lineType=8, shift=0)
+                prev = cur
+
+        print(f"Path found: {shortest}p in order {order}")
+
+        end = timeit.default_timer()
+        print(f"Pathing done in: {end - start}")
+        return image, order
+    except Exception as e:
+        print(e)
+        return image, []
